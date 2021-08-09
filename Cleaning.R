78f7868678a6b0d45dd9ad30f00b16895ffd6dd2
cyrus-muriithi <karokicyrus@gmail.com>
2021-08-09 08:41:32 +0300

    Updated files

--- Cleaning.R
+++ Cleaning.R
@@ -37,9 +37,9 @@ Main.df[is.na(Main.df)]<-0
 
 
 Main.df<- Main.df %>% 
-  mutate(recovered = ifelse(date=="2021-8-5",195685,
-                            ifelse(date=="2021-8-6",195787,
-                                   ifelse(date=="2021-8-7",197029,recovered))))
+  mutate(recovered = ifelse(date=="2021-8-5" & Country=="Kenya",195685,
+                            ifelse(date=="2021-8-6" & Country=="Kenya",195787,
+                                   ifelse(date=="2021-8-7" & Country=="Kenya",197029,recovered))))
                             
   
 #-------------------------------------------------------------------------------------------
